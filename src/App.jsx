efff594c7641d416e2a95524a9496e24fb95b766
Kyle <kylewhitlatch@gmail.com>
2025-04-23 01:50:47 -0500

    Added Gallery Zoom and image dragging

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './css/App.css'
-import { Component, createRef } from "react";
+import { Component } from "react";
 import TabHeader from "./components/TabHeader.jsx";
 import HomePage from "./components/pages/HomePage.jsx";
 import GalleryPage from "./components/pages/GalleryPage.jsx";
@@ -7,32 +7,58 @@ import ComicPage from "./components/pages/ComicPage.jsx";
 import FormPage from "./components/pages/FormPage.jsx";
 
 const TABS = {
-  HOME: { index: 0, title: 'home' },
-  GALLERY: { index: 1, title: 'gallery' },
-  COMIC: { index: 2, title: 'comic' },
-  FORM: { index: 3, title: 'form' },
+  HOME: { index: 0, title: 'Home' },
+  GALLERY: { index: 1, title: 'Gallery' },
+  COMIC: { index: 2, title: 'Comic' },
+  FORM: { index: 3, title: 'Form' },
 }
 
 export default class App extends Component {
+  state = {
+    activeTab: TABS.HOME,
+  }
   
   constructor( props ) {
     super( props );
-    createRef()
-    this.state = {
-      activeTab: TABS.HOME,
-    };
     
+    this.activeTab = TABS.HOME;
+    this.checkState();
     this.baseDirectory = import.meta.env.BASE_URL;
+  }
+  
+  componentDidMount() {
+    this.setState( { activeTab: this.activeTab } );
+    window.addEventListener( 'popstate', this.onPopState );
+  }
+  
+  checkState() {
+    let path = window.location.pathname;
+    let activeTab = TABS[ Object.keys(TABS).find( key => path.includes( TABS[key].title ) ) ];
+    if ( activeTab ) {
+      this.activeTab = activeTab;
+    }
   }
   
   onTabChange = tab => {
     let activeTab = TABS[ Object.keys(TABS)[tab] ];
+    this.SetActiveTab( activeTab );
+  }
+  
+  UpdatePage = activeTab => {
+    // This updates the state of the page, and edits the URL, but it breaks github pages to have a URL other than the root
+    // history.pushState(null, activeTab.title, this.baseDirectory + activeTab.title );
+    history.pushState(null, activeTab.title );
+    return null;
+  }
+  
+  SetActiveTab = ( activeTab ) => {
     this.setState({ activeTab: activeTab });
     this.UpdatePage( activeTab );
   }
   
-  UpdatePage = activeTab => {
-    history.pushState(null, activeTab.title, this.baseDirectory + activeTab.title );
+  onPopState = () => {
+    this.checkState();
+    this.setState({ activeTab: this.activeTab });
   }
 
   render() {
--- src/components/pages/GalleryPage.jsx
+++ src/components/pages/GalleryPage.jsx
@@ -1,11 +1,29 @@
-﻿import { Component } from "react";
+﻿import { Component, createRef } from "react";
 import PropTypes from "prop-types";
 
 export default class GalleryPage extends Component {
+  state = {
+    shadowBoxImage: null,
+    currentZoom: 1,
+    stepSize: 0.1,
+    minZoom: 0.6,
+    maxZoom: 3,
+    minDrag: 5,
+    isDragging: false,
+    dragStartX: 0,
+    dragStartY: 0,
+    dragStartOffsetX: 0,
+    dragStartOffsetY: 0,
+    imageOffsetX: 0,
+    imageOffsetY: 0,
+    windowOffset: null,
+  }
+  
   constructor( props ) {
     super( props );
     
     this.filesByDirectory = this.getFilesByDirectory();
+    this.shadowBoxImageRef = createRef();
   }
   
   getFilesByDirectory() {
@@ -33,11 +51,150 @@ export default class GalleryPage extends Component {
     return filesByDirectory;
   }
   
+  shadowBoxImage = ( file ) => {
+    let windowOffset = window.scrollY;
+    this.setState( {
+      shadowBoxImage: file,
+      currentZoom: 1,
+      imageOffsetX: 0,
+      imageOffsetY: 0,
+    } );
+    
+    if ( file ) {
+      document.body.setAttribute( 'style',
+        `position: fixed; top: -${windowOffset}px; left: 0; right: 0;`);
+      this.setState( {
+        windowOffset: windowOffset,
+      } );
+    } else {
+      document.body.setAttribute( 'style', '' );
+      window.scrollTo( 0, this.state.windowOffset );
+      this.setState( {
+        windowOffset: null,
+        imageOffsetX: 0,
+        imageOffsetY: 0,
+      } );
+    }
+  }
+  
+  onScrollImage = ( event ) => {
+    let direction = event.deltaY > 0 ? -1 : 1;
+    let newZoom = this.state.currentZoom + direction * this.state.stepSize;
+    newZoom = Math.max( this.state.minZoom, Math.min( newZoom, this.state.maxZoom ) );
+    this.setState( {
+      currentZoom: newZoom
+    }, this.transformImage );
+  }
+  
+  onImageClick = ( event ) => {
+    event.stopPropagation();
+    this.setState({
+      currentZoom: 1,
+    }, this.transformImage );
+  }
+  
+  onMouseDownImage = (event) => {
+    if (this.state.currentZoom > 1) {
+      this.setState({
+        isDragging: true,
+        dragStartX: event.clientX,
+        dragStartY: event.clientY,
+        dragStartOffsetX: event.clientX,
+        dragStartOffsetY: event.clientY,
+        imageOffsetX: this.state.imageOffsetX || 0,
+        imageOffsetY: this.state.imageOffsetY || 0,
+      });
+      this.shadowBoxImageRef.current.style.cursor = 'grabbing';
+    } else {
+      this.onImageClick(event);
+    }
+  };
+  
+  onMouseMoveImage = (event) => {
+    if (this.state.isDragging) {
+      const deltaX = (event.clientX - this.state.dragStartOffsetX) / this.state.currentZoom;
+      const deltaY = (event.clientY - this.state.dragStartOffsetY) / this.state.currentZoom;
+      
+      this.setState({
+        imageOffsetX: this.state.imageOffsetX + deltaX,
+        imageOffsetY: this.state.imageOffsetY + deltaY,
+        dragStartOffsetX: event.clientX,
+        dragStartOffsetY: event.clientY,
+      }, this.transformImage);
+      
+      // this.transformImage();
+    }
+  };
+  
+  onMouseUpImage = (event ) => {
+    if (this.state.isDragging) {
+      const deltaX = Math.abs(event.clientX - this.state.dragStartX);
+      const deltaY = Math.abs(event.clientY - this.state.dragStartY);
+      
+      if (deltaX < this.state.minDrag && deltaY < this.state.minDrag) {
+        // Treat as a click
+        this.onImageClick(event);
+      }
+      
+      this.setState({
+        isDragging: false,
+      });
+      this.shadowBoxImageRef.current.style.cursor = 'grab';
+    }
+  };
+  
+  transformImage = () => {
+    const container = this.shadowBoxImageRef.current.parentElement;
+    const image = this.shadowBoxImageRef.current;
+
+    const scaledWidth = image.offsetWidth * this.state.currentZoom;
+    const scaledHeight = image.offsetHeight * this.state.currentZoom;
+    
+    const maxOffsetX = Math.max(0, (scaledWidth - container.offsetWidth) / ( 2 * this.state.currentZoom ));
+    const maxOffsetY = Math.max(0, (scaledHeight - container.offsetHeight) / ( 2 * this.state.currentZoom ));
+
+    const newOffsetX = Math.max(-maxOffsetX, Math.min(maxOffsetX, this.state.imageOffsetX));
+    const newOffsetY = Math.max(-maxOffsetY, Math.min(maxOffsetY, this.state.imageOffsetY));
+
+    this.setState({
+      imageOffsetX: newOffsetX,
+      imageOffsetY: newOffsetY,
+    }, () => {
+      this.shadowBoxImageRef.current.style.transform = `
+        scale(${this.state.currentZoom})
+        translate(${newOffsetX}px, ${newOffsetY}px)
+      `;
+    });
+  }
+  
   render() {
     return (
       <>
+        {this.state.shadowBoxImage &&
+          <div className={'shadowBox'} onClick={() => { this.shadowBoxImage( null ) }} >
+            <span className={'shadowBoxClose'}>&times;</span>
+            <div className={'shadowBoxImgContainer'} onWheel={ this.onScrollImage }>
+              <img
+                ref={this.shadowBoxImageRef}
+                src={this.state.shadowBoxImage}
+                alt={"Full Screen Image"}
+                className={'shadowBoxContent'}
+                onMouseDown={this.onMouseDownImage}
+                onMouseMove={this.onMouseMoveImage}
+                onMouseUp={this.onMouseUpImage}
+                onMouseLeave={this.onMouseUpImage}
+                onClick={ e => e.stopPropagation() }
+                onDrag={ e => e.preventDefault() }
+                onDragStart={ e => e.preventDefault()}
+              />
+            </div>
+          </div>
+          
+        }
+        
         {Array.from( Object.keys( this.filesByDirectory ) ).map( ( directory, index ) => {
-          return <GalleryCard directory={directory} files={this.filesByDirectory[directory]} key={index}/>
+          return <GalleryCard directory={directory} files={this.filesByDirectory[directory]} key={index}
+            onClickCallback={ this.shadowBoxImage } />
         } )}
       </>
     )
@@ -61,20 +218,28 @@ class GalleryCard extends Component {
   }
   
   render() {
-    let baseUrl = (import.meta.env.BASE_URL).slice(0, -1);
+    let baseUrl = ( import.meta.env.BASE_URL ).slice( 0, -1 );
     
     return (
       <div className="card">
         <h1>{this.title}</h1>
         <div className={'galleryContainer'}>
           {Object.values( this.files ).map( ( file, index ) => {
+            let filepath = baseUrl + file;
             if ( import.meta.env.PROD ) {
-              const parts = file.split('/'); // Split the string into an array
-              const filteredParts = parts.filter(part => part !== 'public'); // Remove 'public'
-              file = filteredParts.join('/'); // Reconstruct the string
+              const parts = file.split( '/' ); // Split the string into an array
+              const filteredParts = parts.filter( part => part !== 'public' ); // Remove 'public'
+              filepath = filteredParts.join( '/' ); // Reconstruct the string
             }
             
-            return ( <img className={'galleryImg'} alt={"Teef's wonderful art"} src={baseUrl + file + '?url'} key={index}/> )
+            return (
+              <img
+                className={'galleryImg'}
+                alt={"Teef's wonderful art"}
+                src={filepath}
+                onClick={ () => this.props.onClickCallback(filepath) }
+                key={index}
+              /> )
           } )}
         </div>
       
@@ -87,4 +252,5 @@ class GalleryCard extends Component {
 GalleryCard.propTypes = {
   directory: PropTypes.string.isRequired,
   files: PropTypes.array.isRequired,
+  onClickCallback: PropTypes.any,
 };
